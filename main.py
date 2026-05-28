f6300b079b6809b28ded2c87421912c1762d9563
Isaac Alencar <88695345+usuario934@users.noreply.github.com>
2026-05-28 18:51:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,26 @@ def home():
 @app.get("/usuario/{nome}")
 def usuario(nome: str):
     return {"msn": f"Ola {nome}"}
+
+
+@app.get("/authenticate/{id}")
+def auth(id: str):
+
+    __list_authentic = [
+        {
+            "key": "1234",
+            "status": "ativado"
+        },
+        {
+            "key": "uni_micro_centro",
+            "status": "ativado"
+        },
+        {
+            "key": "uni_micro_cidade_nova",
+            "status": "desativado"
+        }
+    ]
+
+    for item in __list_authentic:
+        if item.get("key") == id:
+            return item
